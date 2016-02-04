cc014cdedbe7f504db9d44f3e3f09b4b21a6a14d
Jody Hamilton <jody@zivtech.com>
2016-02-04 18:19:27 -0500

    Drupal 8.0.3 with some behat tests and a probo build config

---
+++ tests/README.md
@@ -0,0 +1,5 @@
+1. To set up test framework, in this directory run: composer install
+
+2. Edit behat.yml to set the base_url and webroot properly.
+
+3. Run tests with bin/behat
---
+++ tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,23 @@
+<?php
+
+use Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+use Behat\Gherkin\Node\TableNode;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct() {
+    }
+}
